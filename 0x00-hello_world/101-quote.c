d294c3b916bac71981010d84a2b5efcaa114430c
anyango-okoth <anyangomoureen02@gmail.com>
2023-04-22 12:55:47 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - A program that prints a line for a standard error
+ *
+ * Return: 1 (Success)
+ */
+int main(void)
+{
+fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n” , stdout);
+return (1);
+}
+
