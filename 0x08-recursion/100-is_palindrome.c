f612880ad8f3757a47ea28c7840b05be4ce22848
anyango-okoth <anyangomoureen02@gmail.com>
2023-05-11 13:26:18 -0700

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -35,10 +35,11 @@ return (1 + _strlen_recursion(s + 1));
  */
 int check_pal(char *s, int i, int len)
 {
-if (*(s + i) != *(s + len - 1)
+if (*(s + i) != *(s + len - 1))
 return (0);
 if (i >= len)
 return (1);
 return (check_pal(s, i + 1, len - 1));
+}
 }
 
