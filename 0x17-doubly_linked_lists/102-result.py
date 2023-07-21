48e79250fe814c90fed69e5564d5a854b9a1f4fb
anyango-okoth <anyangomoureen02@gmail.com.com>
2023-07-21 14:02:16 -0700

    palindrome

---
+++ 0x17-doubly_linked_lists/102-result.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+def is_palindrome(n):
+    return str(n) == str(n)[::-1]
+
+def find_largest_palindrome():
+    largest_palindrome = 0
+    for i in range(100, 1000):
+        for j in range(i, 1000):
+            product = i * j
+            if is_palindrome(product) and product > largest_palindrome:
+                largest_palindrome = product
+    return largest_palindrome
+
+# Find the largest palindrome
+result = find_largest_palindrome()
+
+# Save the result in the file "102-result"
+with open("102-result", "w") as file:
+    file.write(str(result))
+
