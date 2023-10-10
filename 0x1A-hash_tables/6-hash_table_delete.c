a9648582d5b552b8861b1a0b289f3bdf989b5792
anyango-okoth <anyangomoureen02@gmail.com>
2023-10-10 06:29:42 -0700

    delete-hashtable

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - Deletes a hash table.
+ * @ht: A pointer to a hash table.
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_table_t *head = ht;
+	hash_node_t *node, *temp;
+	unsigned long int n;
+
+	for (n = 0; n < ht->size; n++)
+	{
+		if (ht->array[n] != NULL)
+		{
+			node = ht->array[n];
+			while (node != NULL)
+			{
+				temp = node->next;
+				free(node->key);
+				free(node->value);
+				free(node);
+				node = temp;
+			}
+		}
+	}
+	free(head->array);
+	free(head);
+}
